bc8257925ad1f038280360284e4db5854447fc53
Pansilu Gunaratna <62112602+pansilugunaratna@users.noreply.github.com>
2021-10-31 12:53:19 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -11,6 +11,30 @@ void bubble_sort(int a[], int n) {
         }
     }
 }
+void quicksort(int number[25], int first, int last) {
+    int i, j, pivot, temp;
+    if (first < last) {
+        pivot = first;
+        i = first;
+        j = last;
+        while (i < j) {
+            while (number[i] <= number[pivot] && i < last)
+                i++;
+            while (number[j] > number[pivot])
+                j--;
+            if (i < j) {
+                temp = number[i];
+                number[i] = number[j];
+                number[j] = temp;
+            }
+        }
+        temp = number[pivot];
+        number[pivot] = number[j];
+        number[j] = temp;
+        quicksort(number, first, j - 1);
+        quicksort(number, j + 1, last);
+    }
+}
 int main() {
   int a[100], n, i, d, swap;
   printf("Enter number of elements in array:\n");
